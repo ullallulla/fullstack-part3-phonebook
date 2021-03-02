a8ec8c7d379d381c8f78477386013e97b6b3a7ac
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-03-02 14:32:54 +0200

    Added delete functionality

--- index.js
+++ index.js
@@ -93,6 +93,13 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
+  })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
